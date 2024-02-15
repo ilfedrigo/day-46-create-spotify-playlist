21b3b383143771d48beef339681dcfac7567c625
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-15 10:22:36 -0300

    Final implementation of the Spotify 100 Billboard songs Generator

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ date = input("Which year to you want to travel to? Type the date in this format 
 
 URL = "https://www.billboard.com/charts/hot-100/"
 
-CLIENT_ID = "8d66cf5f7cb54a4680aff361f97dbbf8"
-CLIENT_SECRET = "8c4f7b24a13b4c308eb38ea49efbab60"
-
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+APP_URI = "https://example.com/"
 response = requests.get(URL+date)
 billboard = response.text
 soup = BeautifulSoup(billboard, 'html.parser')
@@ -17,10 +17,22 @@ soup = BeautifulSoup(billboard, 'html.parser')
 top_100_songs = soup.select("li ul li h3")
 song_list = [song.getText().strip() for song in top_100_songs]
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, 
-                                                           client_secret=CLIENT_SECRET, 
-                                                           redirect_uri="https://example.com/", 
-                                                           scope="playlist-modify-private",
-                                                           username="lucas.fedrigo"))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth
+                                    (
+                                    client_id=CLIENT_ID, 
+                                    client_secret=CLIENT_SECRET, 
+                                    redirect_uri=APP_URI, 
+                                    scope="playlist-modify-public",
+                                    username="lucas.fedrigo"
+                                    )
+                    )
 
 user_id = sp.current_user()["id"]
+
+uris = [sp.search(title)["tracks"]["items"][0]["uri"] for title in song_list]
+
+# Create playlist:
+playlist_id = sp.user_playlist_create(user=user_id, name=f"{date} - Billboard 100", public=True, description='')['id']
+
+# Insert Tracks on Playlist
+sp.user_playlist_add_tracks(playlist_id=playlist_id, tracks=uris, user=user_id)
